71b97150bd0ba7cccda83c3916c697f5aee31c6f
MethmaW <methma.cw@gmail.com>
2021-06-18 16:03:11 +0530

    Initial commit

---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-mods-home",
+	templateUrl: "./mods-home.component.html",
+	styleUrls: ["./mods-home.component.css"],
+})
+export class ModsHomeComponent implements OnInit {
+	modalOpen = false;
+	items = [
+		{
+			title: "Why is the sky blue?",
+			content: "Sky is blue because it is",
+		},
+		{
+			title: "What does an orange taste like",
+			content: "An orange tastes like orange",
+		},
+		{
+			title: "What color is that cat",
+			content: "The ct is an orange color",
+		},
+	];
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	onClick() {
+		this.modalOpen = !this.modalOpen;
+	}
+}
